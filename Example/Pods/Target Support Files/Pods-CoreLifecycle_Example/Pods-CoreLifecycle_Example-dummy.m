3d708d115c02b5c3fb639fa3066a850e6bf5d588
shoutokobayashi <deepstrap353@gmail.com>
2020-06-15 17:38:35 +0900

    first commit

---
+++ Example/Pods/Target Support Files/Pods-CoreLifecycle_Example/Pods-CoreLifecycle_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CoreLifecycle_Example : NSObject
+@end
+@implementation PodsDummy_Pods_CoreLifecycle_Example
+@end
---
+++ Example/Pods/Target Support Files/Pods-CoreLifecycle_Tests/Pods-CoreLifecycle_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CoreLifecycle_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_CoreLifecycle_Tests
+@end
